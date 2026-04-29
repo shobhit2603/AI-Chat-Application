c4ce6fadecd7f2af5b9cdc9904bfe59f65883e00
Shobhit Shrivastava <shrivastavashobhit706@gmail.com>
2026-04-29 11:39:45 +0530

    Initial Commit

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='min-h-screen flex items-center justify-center'>
-      <h1 className='text-5xl font-light'>Chat App</h1>
+      <h1 className='text-5xl font-light'>Gen AI Chat Application.</h1>
     </div>
   )
 }
